41c56070758bcdffbae89936124c70c55dc5532b
Hamish Hutchings <moredhel@aoeu.me>
2018-06-08 15:14:13 +0200

    cleanup, now runs correctly

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
 provider "aws" {
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
+  version = "~> 1.22"
 }
 
 resource "aws_ecs_cluster" "unifargate" {
   name = "unifargate"
 }
 
-resource "aws_task_definition" "unifitask" {
+resource "aws_ecs_task_definition" "unifitask" {
   family                = "unifi"
   container_definitions = "${file("containerdefinition.json")}"
 }
 
 resource "aws_ecs_service" "unifiservice" {
   name            = "unificontroller"
-  cluster         = "${aws_ecs_cluster.unifargate}"
-  task_definition = "${aws_task_definition.unifitask.arn}"
+  cluster         = "${aws_ecs_cluster.unifargate.name}"
+  task_definition = "${aws_ecs_task_definition.unifitask.arn}"
   desired_count   = 1
 
   #iam_role
